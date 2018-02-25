a714bcaf85a59548b6ab89b1cce9218dccddef82
Kevin Cooper <cooperka92@gmail.com>
2018-02-25 17:18:52 -0500

    :fire: Remove 'coming soon' footer

--- src/components/Footer/component.js
+++ src/components/Footer/component.js
@@ -7,7 +7,6 @@ import './styles.css';
 function Footer() {
   return (
     <div className="Footer">
-      <div>Full website coming soon.</div>
       <div>&copy; 2018 Kevin Cooper</div>
     </div>
   );
