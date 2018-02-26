05088ea51b36081f03ae7e166e2e4226ef03adb4
Kevin Cooper <cooperka92@gmail.com>
2018-02-25 19:51:35 -0500

    :art: Add image source copyright notice to footer

--- src/components/Footer/component.js
+++ src/components/Footer/component.js
@@ -8,6 +8,7 @@ function Footer() {
   return (
     <div className="Footer">
       <div>&copy; 2018 Kevin Cooper</div>
+      <div>Images are sourced from Wikipedia and may be subject to copyright.</div>
     </div>
   );
 }
