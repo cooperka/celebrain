bb873236abd26ddde25788345bd448901dfdfbb3
Kevin Cooper <cooperka92@gmail.com>
2018-08-30 22:48:08 -0400

    :package: Make footer height a constant

--- src/components/Footer/component.js
+++ src/components/Footer/component.js
@@ -1,19 +1,25 @@
 // @flow
 
 import * as R from 'ramda';
+import classNames from 'classnames';
 import React, { Component } from 'react';
 import { withStyles } from 'material-ui';
+
+import { size } from '../../constants';
 
 import './styles.css';
 
 type Props = {
+  classes: Object,
 };
 
 class Footer extends Component<Props> {
 
   render() {
+    const { classes } = this.props;
+
     return (
-      <div className="Footer">
+      <div className={classNames(classes.footer, 'Footer')}>
         <div>&copy; 2018 Kevin Cooper</div>
         <div>Images are sourced from Wikipedia and may be subject to copyright.</div>
       </div>
@@ -23,6 +29,9 @@ class Footer extends Component<Props> {
 }
 
 const styles = () => ({
+  footer: {
+    height: size.FOOTER_HEIGHT,
+  },
 });
 
 export default R.compose(
