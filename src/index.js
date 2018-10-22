bb98e0218c994d61d309afd8eda4a2d7a59f519e
Kevin Cooper <cooperka92@gmail.com>
2018-10-22 14:07:31 -0400

    :package: Update index.js via LSR

--- src/index.js
+++ src/index.js
@@ -9,30 +9,27 @@ import App from './components/App/component';
 
 import './styles.css';
 
-// Enable Hot Module Replacement (HMR) for rendering.
-if (module.hot) {
-  module.hot.accept('./components/App/component', () => {
-    // eslint-disable-next-line global-require
-    const UpdatedApp = require('./components/App/component').default;
-    renderRoot(UpdatedApp);
-  });
-}
-
-renderRoot(App);
-
-function renderRoot(AppComponent) {
+function renderRoot(component) {
   // Only if document is available; skip during static builds.
   if (typeof document !== 'undefined') {
     // $FlowFixMe
     const renderMethod = module.hot ? ReactDOM.render : ReactDOM.hydrate;
 
-    renderMethod(
-      <AppComponent />,
-      // $FlowFixMe
-      document.getElementById('root'),
-    );
+    // $FlowFixMe
+    renderMethod(component, document.getElementById('root'));
   }
 }
+
+// Enable Hot Module Replacement (HMR).
+if (module.hot) {
+  module.hot.accept('./components/App/component', () => {
+    // eslint-disable-next-line global-require
+    const UpdatedApp = require('./components/App/component').default;
+    renderRoot(<UpdatedApp />);
+  });
+}
+
+renderRoot(<App />);
 
 // Export top level component for static rendering.
 export default App;
